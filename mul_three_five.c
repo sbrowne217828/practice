26b1cfde5345499f19becdc0038ea204ea6ced8d
sbrowne217828 <sbrowne217828@gmail.com>
2017-08-01 23:18:41 -0700

    practice coding in bodhi

---
+++ mul_three_five.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int	multhreefive(int n)
+{
+	int	i;
+	int	nbr;
+
+	
+	i = 0;
+	nbr = 0;
+	while (i < n)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			nbr = nbr + i++;
+		else
+			i++;
+		//printf("I equals: %d\n", i);
+		//printf("sum equals: %d\n", nbr);
+	}
+
+	return (nbr);
+}
+
+int main()
+{
+	int	scan;
+	int	sum;
+
+	scan = 0;
+	sum = 0;
+	printf("Input number to calculate the sum of the 3 & 5 multiples: \n");
+	scanf("%d", &scan);
+	sum = multhreefive(scan);
+	printf("Sum of input: %d \n", sum);
+	return (0);
+}
